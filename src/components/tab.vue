1a352933403ee5804c908bffc9c9a45b7bed3766
TuringCodeOne <1962732398@qq.com>
2021-05-12 09:42:53 +0800

    feat: 商品信息录入界面

--- src/components/tab.vue
+++ src/components/tab.vue
@@ -6,7 +6,73 @@
     type="border-card"
     :stretch="true"
   >
-    <el-tab-pane label="农产品信息管理" name="first">农产品数据</el-tab-pane>
+    <el-tab-pane label="农产品信息管理" name="first">
+      <div class="first-container">
+        <div class="first-title">
+          <h1>商品注册表</h1>
+        </div>
+        <el-divider></el-divider>
+        <el-form :model="form" label-width="140px">
+          <el-form-item label="农产品名称: ">
+            <el-input
+              v-model="form.foodName"
+              placeholder="例如: 车厘子"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="广告语: ">
+            <el-input
+              v-model="form.advertiseSlogan"
+              placeholder="例如: 车厘子很好吃！"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="食用方法: ">
+            <el-input
+              v-model="form.eatMethods"
+              placeholder="例如: 开封即食"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="产地: ">
+            <el-input
+              v-model="form.location"
+              placeholder="例如: 江苏省镇江市京口区"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="工艺流程: ">
+            <el-input
+              v-model="form.processFlow"
+              placeholder="例如: 2月种植，4月成熟，人工采摘，工厂化加工"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="贮藏条件: ">
+            <el-input
+              v-model="form.storageCondition"
+              placeholder="例如：常温下20天"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="售后服务说明: ">
+            <el-input
+              v-model="form.afterSalesService"
+              placeholder="例如：3天无理由退货"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="价格说明: ">
+            <el-input
+              v-model="form.priceDescription"
+              placeholder="例如：价格中附带运费"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="产品详细介绍文件: ">
+            <el-input
+              v-model="form.detailFoodFileHref"
+              placeholder="TODO: 文件hash地址"
+            ></el-input>
+          </el-form-item>
+          <el-button class="first-sumbit" type="primary" @click="onSubmit" plain
+            >提交申请</el-button
+          >
+        </el-form>
+      </div>
+    </el-tab-pane>
     <el-tab-pane label="交易管理" name="second">配置管理</el-tab-pane>
     <el-tab-pane class="el-tab-pane-botton" label="商品溯源" name="third">
       <div class="block">
@@ -44,7 +110,7 @@
               </el-collapse-item>
             </el-collapse>
           </el-timeline-item>
-          <el-timeline-item v-for="(source,index) in sources" :key="index">
+          <el-timeline-item v-for="(source, index) in sources" :key="index">
             <h3>{{ source.time }}</h3>
             <h4>交易描述:</h4>
             <p class="content-backspace">
@@ -67,8 +133,18 @@ export default {
   name: "Tab",
   data() {
     return {
-      searchFoodId: '',
-      activeName: "third",
+      form: {
+        foodName: "",
+        advertiseSlogan: "",
+        eatMethods: "",
+        processFlow: "",
+        storageCondition: "",
+        afterSalesService: "",
+        priceDescription: "",
+        detailFoodFileHref: "",
+      },
+      searchFoodId: "",
+      activeName: "first",
       foodinfo: {
         foodId: "0xdfdsfsdf",
         foodName: "l",
@@ -120,5 +196,15 @@ export default {
 }
 .content-block {
   width: 100%;
+}
+
+.first-sumbit {
+  width: 100%;
+  margin-bottom: 20px;
+}
+.first-title {
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 </style>
